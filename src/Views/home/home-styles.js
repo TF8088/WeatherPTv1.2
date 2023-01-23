0d1772204d7da59144db77b931c26ed74a32120b
TF8088 <tiagofarinha03@outlook.com>
2023-01-23 20:01:58 +0000

    home page underdevelopment

--- src/Views/home/home-styles.js
+++ src/Views/home/home-styles.js
@@ -1,10 +1,33 @@
 import { StyleSheet } from "react-native";
-
+import colors, { coolGray } from 'tailwindcss/colors';
 const styles = StyleSheet.create({
-    view: {
+    container: {
         flex: 1,
-        backgroundColor: 'red'
-    }
+        backgroundColor: colors.sky[500],
+        paddingTop: 40,
+    },
+    header: {
+        alignItems: 'flex-start',
+        alignContent: "center",
+        flexDirection: 'row',
+        marginTop: 15,
+        paddingHorizontal: 20,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    citytittle: {
+        fontSize: 35,
+        fontWeight: '950',
+        color: '#fff',
+    },
+    storyView: {
+        paddingVertical: 4,
+        marginTop: 4,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: colors.neutral[100],
+    },       
 });
 
 export default styles
--- src/Views/home/home.js
+++ src/Views/home/home.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { View } from 'react-native';
+// Libs
+import { View, Text, TouchableOpacity, ScrollView, } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
 import styles from './home-styles';
 
 export function Home() {
   return (
-    <View style={styles.view}>
-
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => { /*Para dar refresh na pagina*/ console.log("Refresh"); }} >
+          <Feather name="refresh-cw" size={30} color={colors.neutral[50]} />
+        </TouchableOpacity>
+        <Text style={styles.citytittle}> City </Text>
+        <TouchableOpacity onPress={() => { /*Para dar refresh na pagina*/ console.log("Refresh"); }} >
+          <Feather name="search" size={30} color={colors.neutral[50]} />
+        </TouchableOpacity>
+      </View>
+     
     </View>
   );
 }
+
