4f8cad082b5d6691def51dd6686ce6923f0c7939
TF8088 <tiagofarinha03@outlook.com>
2023-01-23 20:02:30 +0000

    settings page

--- src/Views/settings/settings.js
+++ src/Views/settings/settings.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import styles from './settings-style';
+import styles from './settings-styles';
 
 export function Settings() {
   return (
